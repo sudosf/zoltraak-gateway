a39f68aa455f5bed5c855f4108110110f6c20de0
Siya Nkuna <nkunaf.sf@gmail.com>
2026-05-03 02:10:55 +0200

    remove unnecessary public modifier

--- src/main/java/com/zoltraak/gateway/ZoltraakGatewayApplication.java
+++ src/main/java/com/zoltraak/gateway/ZoltraakGatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZoltraakGatewayApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ZoltraakGatewayApplication.class, args);
     }
 
