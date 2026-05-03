92886ad12e67bde6c25fc4c38c88b65ecd474507
Siya Nkuna <nkunaf.sf@gmail.com>
2026-05-03 02:04:48 +0200

    init project

---
+++ src/main/java/com/zoltraak/gateway/ZoltraakGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zoltraak.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoltraakGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoltraakGatewayApplication.class, args);
+    }
+
+}
