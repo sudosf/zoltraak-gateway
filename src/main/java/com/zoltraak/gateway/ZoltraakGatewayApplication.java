b53baabfd4bd709b9390a04f39b65ef354c1d65a
Siya Nkuna <nkunaf.sf@gmail.com>
2026-07-08 00:09:40 +0200

    chore: exclude UserDetailsService from Spring Boot auto configuration

--- src/main/java/com/zoltraak/gateway/ZoltraakGatewayApplication.java
+++ src/main/java/com/zoltraak/gateway/ZoltraakGatewayApplication.java
@@ -3,11 +3,12 @@ package com.zoltraak.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.security.autoconfigure.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
 @ConfigurationPropertiesScan
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class ZoltraakGatewayApplication {
 
     static void main(String[] args) {
